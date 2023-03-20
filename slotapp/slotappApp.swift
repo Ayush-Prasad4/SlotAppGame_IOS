d4648fcf812bc67b27d704cb4e11a7e8447a4e83
Ayush-Prasad4 <89988134+Ayush-Prasad4@users.noreply.github.com>
2023-03-20 19:15:26 +0530

    Initial Commit

---
+++ slotapp/slotappApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotappApp.swift
+//  slotapp
+//
+//  Created by Ayush Prasad on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct slotappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
